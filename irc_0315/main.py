5032ca2bc242d46db0c1f27c512e6b2c4bd4fe58
Yin-1022 <kv.huang931022@gmail.com>
2024-03-15 18:39:43 +0800

    Popcat

--- irc_0315/main.py
+++ irc_0315/main.py
@@ -1,21 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', value=True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-data = driver.get('https://zh.wikipedia.org/wiki/Wikipedia:首页')
+data = driver.get('https://popcat.click')
 
-Texas = driver.find_element(By.XPATH, '//*[@id="column-feature"]/p/a[1]')
-Texas.click()
-print(driver.title)
-name = driver.find_element(By.XPATH, '//*[@id="firstHeading"]/span[1]')
-print(name.text)
+Click = driver.find_element(By.XPATH, '//*[@id="app"]/div')
 
-next = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[1]/a[3]')
-next.click()
+for i in range(100):
+    Click.click();
+    time.sleep(1);
+
+
+# Texas = driver.find_element(By.XPATH, '//*[@id="column-feature"]/p/a[1]')
+# Texas.click()
+# print(driver.title)
+# name = driver.find_element(By.XPATH, '//*[@id="firstHeading"]/span[1]')
+# print(name.text)
+
+# next = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[1]/a[3]')
+# next.click()
 
 # keyword = driver.find_element(By.XPATH, '//*[@id="searchform"]/div/div/div[1]/input')
 # keyword.click()
