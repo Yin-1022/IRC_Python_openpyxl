6bfd076d6c9ddbe4ba30c6600a2acb93ca551732
Yin-1022 <kv.huang931022@gmail.com>
2024-03-15 17:57:10 +0800

    init project

--- 20240223_First.py
+++ 20240223_First.py
@@ -1,9 +1,1 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Chrome()
-data = driver.get('https://zh.wikipedia.org/wiki/兰顿蚂蚁')
-print(driver.title)
-
-name = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[2]')
-print(name.text)
+print("Hello world")
