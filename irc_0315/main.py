64f0303057ec97870ad54a1238e1dfbc8412d86a
Yin-1022 <kv.huang931022@gmail.com>
2024-03-15 18:32:31 +0800

    Getting

--- irc_0315/main.py
+++ irc_0315/main.py
@@ -1,9 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-data = driver.get('https://zh.wikipedia.org/wiki/兰顿蚂蚁')
-print(driver.title)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', value=True)
 
-name = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[2]')
+driver = webdriver.Chrome(options=chrome_options)
+
+data = driver.get('https://zh.wikipedia.org/wiki/Wikipedia:首页')
+
+Texas = driver.find_element(By.XPATH, '//*[@id="column-feature"]/p/a[1]')
+Texas.click()
+print(driver.title)
+name = driver.find_element(By.XPATH, '//*[@id="firstHeading"]/span[1]')
 print(name.text)
+
+next = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[1]/a[3]')
+next.click()
+
+# keyword = driver.find_element(By.XPATH, '//*[@id="searchform"]/div/div/div[1]/input')
+# keyword.click()
+# keyword.send_keys('USA')
+# Search = driver.find_element(By.XPATH, '//*[@id="searchform"]/div/button')
+# Search.click()
+
+# data = driver.get('https://zh.wikipedia.org/wiki/Wikipedia:首页')
+
+# element = driver.find_element_by_class_name("//*[@id="searchform"]/div/div/div[1]/input")
+# element.send_keys("Selenium Python")
+
+# print(driver.title)
+
+# name = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[2]')
+# print(name.text)
